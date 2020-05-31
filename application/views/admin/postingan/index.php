7225253697596cacbd36c154e23ff52b8a039669
aziyan99 <rajaazian08@gmail.com>
2020-05-31 23:25:08 +0700

    add link to update

--- application/views/admin/postingan/index.php
+++ application/views/admin/postingan/index.php
@@ -40,7 +40,7 @@
 							<td><?= date('D, d-M-Y', $data['tanggal_dibuat']); ?></td>
 							<td>
 								<a href="<?= base_url('postingan/detail/' . $data['id']); ?>" class="btn btn-info btn-sm">Detail</a>
-								<a href="" class="btn btn-warning btn-sm">Edit</a>
+								<a href="<?= base_url('postingan/edit/') . $data['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
 								<a href="" class="btn btn-danger btn-sm">Hapus</a>
 							</td>
 						</tr>
