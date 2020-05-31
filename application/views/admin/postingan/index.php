7e024d187db92fe126a0d18693353a0d5bc929a5
aziyan99 <rajaazian08@gmail.com>
2020-06-01 00:04:00 +0700

    add delete link

--- application/views/admin/postingan/index.php
+++ application/views/admin/postingan/index.php
@@ -41,7 +41,7 @@
 							<td>
 								<a href="<?= base_url('postingan/detail/' . $data['id']); ?>" class="btn btn-info btn-sm">Detail</a>
 								<a href="<?= base_url('postingan/edit/') . $data['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
-								<a href="" class="btn btn-danger btn-sm">Hapus</a>
+								<a onclick="return confirm('Hapus postingan ini ?');" href="<?= base_url('postingan/hapus/') . $data['id']; ?>" class="btn btn-danger btn-sm">Hapus</a>
 							</td>
 						</tr>
 					<?php endforeach; ?>
